25f05fb58c822c998a1e87d04dfccc156621f75e
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-12-20 15:48:59 +0300

    SERVE SETUP

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+console.log(PORT);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+})
